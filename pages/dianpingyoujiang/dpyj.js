85b6685af7212e58c113c592e5133fe21607271f
brightming <minghgu@163.com>
2018-05-01 12:58:26 +0800

    add 点评有奖的每个星星的点评控制

--- pages/dianpingyoujiang/dpyj.js
+++ pages/dianpingyoujiang/dpyj.js
@@ -10,18 +10,18 @@ Page({
   data: {
     fullStar: '../../images/icon-start-full.png',
     emptyStar: '../../images/icon-start-empty.png',
-    userStars: [
-      '../../images/icon-start-full.png',
-      '../../images/icon-start-empty.png',
-      '../../images/icon-start-empty.png',
-      '../../images/icon-start-empty.png',
-      '../../images/icon-start-empty.png'
-    ],
     numScores:[
       0,1,2,3,4,5,6,7,8,9,10
     ],
-    dpyjDatas:[
-    ]
+    dpyjDatas: [
+    ],
+    dpyjStars: [//二维数组，每个数组对应点评模板的一项，让各自可以独立开来，在得到dpyjDatas的时候创建
+
+    ],
+    dpyjScores: [//对应的分数
+
+    ],
+    extraDesc: ''//额外的评价
   },
 
   /**
@@ -32,18 +32,50 @@ Page({
     console.log("tableInfo=",tableInfo);
     var that=this;
 
-    var bindTemplate=function(datas){
 
-    }
+    var createdata=function(datas) {
+      var dpsj = [];
+      var itemIcons = [];
+      var scores = [];
+      var dataLen=datas.length;
+      //构建训练列表的字段，除了后台返回的数据外，再加上：currentScore,iconList,scoreList，其中iconList,socreList是
+      //用于在循环中显示图标或数字用的
+      for (var i = 0; i < dataLen;i++) {
+        dpsj.push(datas[i]);
+        dpsj[i].currentScore=1;//默认当前得分是1
+        dpsj[i].iconList=[];
+        dpsj[i].scoreList=[];
+        dpsj[i].viewId = 'view-' + datas[i].id; //字符串id，用于页面的id
+        var len = datas[i].level;
+        for (var k = 0; k < len; k++) {
+          dpsj[i].scoreList.push(k);
+          if(k==0){
+            dpsj[i].iconList.push(that.data.fullStar);
+            continue;
+          }
+          dpsj[i].iconList.push(that.data.emptyStar);
+        }
+      }
+      console.log("dpsj=", dpsj);
+      console.log("itemIcons=", itemIcons);
+      console.log("scores=", scores);
+
+      that.setData({
+        dpyjDatas: dpsj //评价
+      })
+      console.log("dpyjDatas=", that.data.dpyjDatas)
+     }
 
      wx.request({
        url: config.service.remarkTemplate,
        method:'GET',
        success:function(res){
          console.log("get remarkTemplate res=",res);
-         that.setData({
-           dpyjDatas:res.data.data
-         })
+         console.log("remarktemp data=",res.data.data)
+        // that.setData({
+        //   dpyjDatas:res.data.data
+        // })
+         createdata(res.data.data);
        }
      })
   },
@@ -98,28 +130,55 @@ Page({
   },
   // 星星点击事件
   starTap: function (e) {
-    console.log("starTap e = ",e);
+    console.log("starTap e = ", e);
+    var viewId = e.currentTarget.id;
     var index = e.currentTarget.dataset.index; // 获取当前点击的是第几颗星星
-    var tempUserStars = this.data.userStars; // 暂存星星数组
-    var len = tempUserStars.length; // 获取星星数组的长度
-    for (var i = 0; i < len; i++) {
-      if (i <= index) { // 小于等于index的是满心
-        tempUserStars[i] = '../../images/icon-start-full.png'
-      } else { // 其他是空心
-        tempUserStars[i] = '../../images/icon-start-empty.png'
+    console.log("viewId=", viewId, "index=", index)
+    var tempDatas = this.data.dpyjDatas; // 暂存星星数组
+
+    //看是对应的哪一个点评模板
+    var dataLen = tempDatas.length;
+    var dataIdx=0;
+    for(var i=0;i<dataLen;i++){
+      if (tempDatas[i].viewId == viewId){
+        dataIdx=i;
+        break;
       }
     }
+    console.log("find data index=",dataIdx);
+    var len = tempDatas[dataIdx].iconList.length; // 获取星星数组的长度
+    for (var i = 0; i < len; i++) {
+      if (i <= index) { // 小于等于index的是满心
+        tempDatas[dataIdx].iconList[i] = '../../images/icon-start-full.png'
+      } else { // 其他是空心
+        tempDatas[dataIdx].iconList[i] = '../../images/icon-start-empty.png'
+      }
+    }
+    //当前分数
+    tempDatas[dataIdx].currentScore=index+1;//index是从0开始的，但是对于星星类型的评分，是从1开始的
+
     // 重新赋值就可以显示了
+    console.log("星星点评后 tempDatas=", tempDatas)   
     this.setData({
-      userStars: tempUserStars
+      dpyjDatas: tempDatas
     })
+    console.log("星星点评后 this.data.dpyjDatas=", this.data.dpyjDatas)
   },
 
   /**  评论框的输入 */
   inputExtraDesc:function(e){
     console.log("input =  ", e.detail.value)
+    this.setData({
+      extraDesc: e.detail.value
+    })
   },
   bindFormSubmit: function (e) {
-    console.log("form detail = ",e.detail.value)
+    console.log("bindFormSubmit dpyjScores=", this.data.dpyjScores);
+    console.log("form detail = ", e.detail.value)
+    var datas = {
+      'extraDesc': this.data.extraDesc,
+      'scores': this.data.dpyjScores
+    }
+    console.log("form data:", datas)
   }
 })
--- pages/fish/test.js
+++ pages/fish/test.js
@@ -1,19 +1,41 @@
 
 Page({
-  onLoad:function(){
+  data: {
+    userStars: [
+      '../../images/icon-start-full.png',
+      '../../images/icon-start-empty.png',
+      '../../images/icon-start-empty.png',
+      '../../images/icon-start-empty.png',
+      '../../images/icon-start-empty.png'
+    ],
+    conts:[{ content: '上菜水平' },
+    { content: '服务态度' },
+    { content: '菜品' }
+    ],
+    dpyjDatas: [
+    ],
+    dpyjStars:[//二维数组，每个数组对应点评模板的一项，让各自可以独立开来，在得到dpyjDatas的时候创建
+
+    ],
+    dpyjScores:[//对应的分数
+
+    ],
+    extraDesc:''//额外的评价
+  },
+  onLoad: function () {
     console.log('page on load')
     wx.getSetting({
-      success:(res)=>{
-        console.log("res=",res)
+      success: (res) => {
+        console.log("res=", res)
         var cont
-        for (var r in res.authSetting ){
-            console.log("---",r,'=',res.authSetting[r])
-            cont=cont+r+"="+res.authSetting[r]+"\n"
+        for (var r in res.authSetting) {
+          console.log("---", r, '=', res.authSetting[r])
+          cont = cont + r + "=" + res.authSetting[r] + "\n"
         }
         this.setData({
-          authorDetails:cont
+          authorDetails: cont
         })
-        if(!res.authSetting['scope.userLocation']){
+        if (!res.authSetting['scope.userLocation']) {
           wx.authorize({
             scope: 'scope.userInfo',
             success: () => {
@@ -24,27 +46,98 @@ Page({
       }
     })
   },
-  openSetting:function(){
+  openSetting: function () {
     wx.openSetting({
-    success: (res) => {
-      console.log("res=", res)
-      var cont
-      for (var r in res.authSetting) {
-        console.log("---", r, '=', res.authSetting[r])
-        cont = cont + r + "=" + res.authSetting[r] + "\n"
+      success: (res) => {
+        console.log("res=", res)
+        var cont
+        for (var r in res.authSetting) {
+          console.log("---", r, '=', res.authSetting[r])
+          cont = cont + r + "=" + res.authSetting[r] + "\n"
+        }
+        this.setData({
+          authorDetails: cont
+        })
       }
-      this.setData({
-        authorDetails: cont
-      })
-    }
     })
   },
-  authorize:function(){
+  authorize: function () {
     wx.authorize({
-      scope: ['scope.writePhotosAlbum','scope.address'],
-      success(res){
-        console.log('get authorize') 
+      scope: 'scope.address',
+      success(res) {
+        console.log('get authorize')
       }
     })
+  },
+
+  // 星星点击事件
+  starTap: function (e) {
+    console.log("starTap e = ", e);
+    var itemId=e.currentTarget.id;
+    var index = e.currentTarget.dataset.index; // 获取当前点击的是第几颗星星
+    console.log("itemId=", itemId, "index=", index)
+
+    var tempUserStars = this.data.dpyjStars; // 暂存星星数组
+    this.data.dpyjScores[itemId]=index+1; //设置得分
+
+    var len = tempUserStars[itemId].length; // 获取星星数组的长度
+
+    for (var i = 0; i < len; i++) {
+      if (i <= index) { // 小于等于index的是满心
+        tempUserStars[itemId][i] = '../../images/icon-start-full.png'
+      } else { // 其他是空心
+        tempUserStars[itemId][i] = '../../images/icon-start-empty.png'
+      }
+    }
+    // 重新赋值就可以显示了
+    console.log("星星点评后 this.data.dpyjScores=", this.data.dpyjScores)
+    this.setData({
+      dpyjStars: tempUserStars
+    })
+  },
+
+  createdata:function(){
+    var conts=this.data.conts;
+    console.log("conts=",this.data.conts)
+    var dpsj=[];
+    var itemstars=[];
+    var scores=[];
+    for(var i in this.data.conts){
+      console.log("i=",i,",content=",this.data.conts[i].content);
+      dpsj.push({ 'id': i, 'seq': i, "content": this.data.conts[i].content});
+      scores.push(1);
+      itemstars.push([]);
+      var len = this.data.userStars.length;
+      for (var k=0;k<len;k++){
+        itemstars[i].push(this.data.userStars[k]);
+      }
+    }
+    console.log("dpsj=",dpsj);
+    console.log("itemstars=", itemstars);
+    console.log("scores=", scores);
+
+    this.setData({
+      dpyjDatas:dpsj,
+      dpyjStars:itemstars,
+      dpyjScores:scores
+    })
+    console.log("dpyjScores=", this.data.dpyjScores)
+  },
+
+  /**  评论框的输入 */
+  inputExtraDesc: function (e) {
+    console.log("input =  ", e.detail.value)
+    this.setData({
+      extraDesc:e.detail.value
+    })
+  },
+  bindFormSubmit: function (e) {
+    console.log("bindFormSubmit dpyjScores=", this.data.dpyjScores);
+    console.log("form detail = ", e.detail.value)
+    var datas={
+      'extraDesc':this.data.extraDesc,
+      'scores':this.data.dpyjScores
+    }
+    console.log("form data:",datas)
   }
 })
